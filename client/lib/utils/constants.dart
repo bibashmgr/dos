53546ce9c25b38dfa5646789c822c91c716febff
Bibash Thapa Magar <bibashthapamagar21@gmail.com>
2022-12-01 19:04:33 +0545

    [add]: constants

--- client/lib/utils/constants.dart
+++ client/lib/utils/constants.dart
@@ -1,1 +1,14 @@
+import 'package:flutter/material.dart';
 
+// colors
+const kLightColor = Color(0xFFFFFFFF);
+const kDarkColor = Color(0xFF020206);
+const kInactiveColor = Color(0xFFF6F6F6);
+const kSelectedColor = Color(0xFF6294F7);
+const kUnselectedColor = Color(0xFFD9D9D9);
+const kHighPriorityColor = Color(0xFFFFC7C7);
+const kMidPriorityColor = Color(0xFFFEF7EC);
+const kLowPriorityColor = Color(0xFFE4F2FB);
+// const kSuccessColor = ;
+// const kFailureColor = ;
+// const kInfoColor = ;
