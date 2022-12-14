212a20307da9ed9ba45ef913993bd2ce488c1baa
Bibash Thapa Magar <bibashthapamagar21@gmail.com>
2022-12-15 00:44:35 +0545

    [update]: constants

--- client/lib/utils/constants.dart
+++ client/lib/utils/constants.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 // colors
 const kLightColor = Color(0xFFFFFFFF);
 const kDarkColor = Color(0xFF020206);
+const kGrayColor = Color(0x77020206);
 const kInactiveColor = Color(0xFFF6F6F6);
 const kSelectedColor = Color(0xFF6294F7);
 const kUnselectedColor = Color(0xFFD9D9D9);
