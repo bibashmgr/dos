9a768a3d9c11f477cd5e4866c8f43eeeffbf1e71
Bibash Thapa Magar <bibashthapamagar21@gmail.com>
2023-01-03 18:11:50 +0545

    [add]: protected routes

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:client/utils/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 // screens
 import 'package:client/screens/main_screen/main.dart';
@@ -7,14 +10,51 @@ import 'package:client/screens/starting_screen/register.dart';
 import 'package:client/screens/starting_screen/login.dart';
 import 'package:client/screens/main_screen/sub_screen/view_project.dart';
 
+// providers
+import 'package:client/providers/user_provider.dart';
+
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => UserProvider()),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-  final bool isLogin = false;
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool isLoaded = false;
+  bool isLogin = false;
+
+  @override
+  void initState() {
+    super.initState();
+    getToken();
+  }
+
+  getToken() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    if (prefs.getString('token') == null || prefs.getString('token')!.isEmpty) {
+      setState(() {
+        isLogin = false;
+        isLoaded = true;
+      });
+    } else {
+      setState(() {
+        isLogin = true;
+        isLoaded = true;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,7 +70,16 @@ class MyApp extends StatelessWidget {
         Register.routeName: (context) => const Register(),
         Login.routeName: (context) => const Login(),
       },
-      home: isLogin ? const MainScreen() : const StartingScreen(),
+      home: Visibility(
+        visible: isLoaded,
+        replacement: const Scaffold(
+            body: Center(
+          child: CircularProgressIndicator(
+            color: kDarkColor,
+          ),
+        )),
+        child: isLogin ? const MainScreen() : const StartingScreen(),
+      ),
     );
   }
 }
