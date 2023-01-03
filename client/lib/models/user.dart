fbc49f0b9df717b67be609a430a588d738f02f99
Bibash Thapa Magar <bibashthapamagar21@gmail.com>
2023-01-03 18:12:23 +0545

    [add]: models for user

---
+++ client/lib/models/user.dart
@@ -0,0 +1,44 @@
+class User {
+  User({
+    required this.data,
+    required this.message,
+  });
+
+  Data data;
+  String message;
+
+  factory User.fromJson(Map<String, dynamic> json) => User(
+        data: Data.fromJson(json["data"]),
+        message: json["message"],
+      );
+}
+
+class Data {
+  Data({
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.avatar,
+    required this.createdAt,
+    required this.updatedAt,
+    required this.v,
+  });
+
+  String id;
+  String name;
+  String email;
+  String avatar;
+  DateTime createdAt;
+  DateTime updatedAt;
+  int v;
+
+  factory Data.fromJson(Map<String, dynamic> json) => Data(
+        id: json["_id"],
+        name: json["name"],
+        email: json["email"],
+        avatar: json["avatar"],
+        createdAt: DateTime.parse(json["createdAt"]),
+        updatedAt: DateTime.parse(json["updatedAt"]),
+        v: json["__v"],
+      );
+}
